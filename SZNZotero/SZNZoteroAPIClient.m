e6c80605cc54364d50cf1eb98837d124078ddab1
vtourraine <me@vtourraine.net>
2013-12-18 11:22:47 +0100

    Fix compiler warning

--- SZNZotero/SZNZoteroAPIClient.m
+++ SZNZotero/SZNZoteroAPIClient.m
@@ -395,7 +395,7 @@ static NSDictionary * AFParametersFromQueryString(NSString *queryString) {
 - (NSString*)MD5
 {
     unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(self.bytes, self.length, md5Buffer);
+    CC_MD5(self.bytes, (CC_LONG)self.length, md5Buffer);
     NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
         [output appendFormat:@"%02x", md5Buffer[i]];
